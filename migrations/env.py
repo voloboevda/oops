a66ae75050ea1e8f2b35615ad3e37c4ffc62e13f
voloboevda <voloboevda@yandex.ru>
2024-06-19 23:02:47 +0500

    add first migration

---
+++ migrations/env.py
@@ -0,0 +1,97 @@
+import os
+import getpass
+from logging.config import fileConfig
+
+from sqlalchemy import engine_from_config
+from sqlalchemy import pool
+from alembic import context
+
+# this is the Alembic Config object, which provides
+# access to the values within the .ini file in use.
+config = context.config
+
+db_dsn = os.environ.get('DB_URL')
+if db_dsn is None:
+    db_user = os.environ.get('APP_DB_USERNAME') or 'postgres'
+    db_name = os.environ.get('APP_DB_NAME') or 'books'
+    db_password = os.environ.get('APP_DB_PASSWORD')
+    db_host = os.environ.get('APP_DB_HOST') or '127.0.0.1'
+    db_port = int(os.environ.get('APP_DB_PORT') or 5432)
+
+    db_dsn = 'postgresql://%s@%s:%d/%s' % (db_user, db_host, db_port, db_name)
+    if db_password:
+        db_password = ':' + db_password
+    if not db_password:
+        db_dsn = 'postgresql://%s@%s:%d/%s' % (db_user, db_host, db_port, db_name)
+    else:
+        db_dsn = 'postgresql://%s%s@%s:%d/%s' % (db_user, db_password, db_host, db_port, db_name)
+
+config.set_main_option("sqlalchemy.url", db_dsn)
+
+# Interpret the config file for Python logging.
+# This line sets up loggers basically.
+fileConfig(config.config_file_name)
+
+# add your model's MetaData object here
+# for 'autogenerate' support
+# from myapp import mymodel
+# target_metadata = mymodel.Base.metadata
+target_metadata = None
+
+
+# other values from the config, defined by the needs of env.py,
+# can be acquired:
+# my_important_option = config.get_main_option("my_important_option")
+# ... etc.
+
+
+def run_migrations_offline():
+    """Run migrations in 'offline' mode.
+
+    This configures the context with just a URL
+    and not an Engine, though an Engine is acceptable
+    here as well.  By skipping the Engine creation
+    we don't even need a DBAPI to be available.
+
+    Calls to context.execute() here emit the given string to the
+    script output.
+
+    """
+    url = config.get_main_option("sqlalchemy.url")
+    context.configure(
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online():
+    """Run migrations in 'online' mode.
+
+    In this scenario we need to create an Engine
+    and associate a connection with the context.
+
+    """
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix="sqlalchemy.",
+        poolclass=pool.NullPool,
+    )
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
+
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
---
+++ migrations/versions/39d892f0aee7_add_books.py
@@ -0,0 +1,37 @@
+"""add books
+
+Revision ID: 39d892f0aee7
+Revises: 
+Create Date: 2024-06-19 21:51:43.276503
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '39d892f0aee7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.execute(
+        '''
+        create table books(
+            id uuid primary key,
+            created_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
+            updated_at timestamp with time zone NOT NULL DEFAULT current_timestamp,
+            title text not null,
+            discription text not null,
+            author text not null,
+            issue_date date not null,
+            page_quantity numeric not null        
+        );
+        '''
+    )
+
+
+def downgrade():
+    raise NotImplementedError
